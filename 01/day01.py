398d41841de0d4bd8135743b259cfdf21112a6d6
konmenel <konmenel@gmail.com>
2022-12-03 22:09:27 +0000

    Day 1 solution

---
+++ 01/day01.py
@@ -0,0 +1,50 @@
+def main() -> int:
+    def _update_logic():
+        if current_cals > most_cals[0]:
+            most_cals[2] = most_cals[1]
+            most_cals[1] = most_cals[0]
+            most_cals[0] = current_cals
+
+            most_cals_elfs[2] = most_cals_elfs[1]
+            most_cals_elfs[1] = most_cals_elfs[0]
+            most_cals_elfs[0] = current_elf
+
+        elif current_cals > most_cals[1]:
+            most_cals[2] = most_cals[1]
+            most_cals[1] = current_cals
+
+            most_cals_elfs[2] = most_cals_elfs[1]
+            most_cals_elfs[1] = current_cals
+
+        elif current_cals > most_cals[2]:
+            most_cals[2] = current_cals
+
+            most_cals_elfs[2] = current_elf
+
+    current_elf = 1
+    current_cals = 0
+    most_cals = [0, 0, 0]
+    most_cals_elfs = [0, 0, 0]
+
+    with open("01/input.txt", "r") as fin:
+        for line in fin.readlines():
+            line = line.strip()
+
+            if not line:
+                print(current_cals)
+                _update_logic()
+
+                current_cals = 0
+                current_elf += 1
+                continue
+
+            current_cals += int(line)
+
+    _update_logic()
+    print(most_cals, most_cals_elfs)
+    print(most_cals[0] + most_cals_elfs[1] + most_cals_elfs[2])
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
